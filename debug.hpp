05afb2497cc30bd1d6e1e3cb0567f1511c165605
bear <bear@prosoftsystems.ru>
2020-10-30 11:21:39 +0500

    Исправить ошибку компиляции в QT
    Signed-off-by: bear <bear@prosoftsystems.ru>

--- debug.hpp
+++ debug.hpp
@@ -9,29 +9,27 @@
 #define DEBUG_HPP_
 
 #include <errno.h>
-#include <assert.h>
+
+#if defined(QT_CORE_LIB)
+  #include <QDebug>
+#else
+  #include <assert.h>
+  #define Q_ASSERT(x) (assert(x))
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if defined(QT_CORE_LIB)
-    #include <QDebug>
-#endif
+  // FIXME В QT флаг NDEBUG не используется. Надо найти его замену!
 
 #if defined(NDEBUG)
 
-  #define Q_ASSERT(x) (void(0))
   #define PRINT_ERROR(x) (void(0))
 
 #else
 
-  #if !defined(QT_CORE_LIB)
-    #define Q_ASSERT(x) (assert(x))
-  #endif
-
   #define PRINT_ERROR(x) printError( __FILE__, __LINE__, __FUNCTION__, x)
-
 
   /** Вывод на экран ошибки и места где она произошла.
    *
@@ -42,6 +40,7 @@ extern "C" {
    */
   void printError(const char *file, int line, const char *function, int error);
 #endif
+
 
 #ifdef __cplusplus
 }
